cfb0be15ff2d3aff0889402ddbd5e5e943794f5f
Vinicius Henrique <viniciushenrique5419@hotmail.com>
2020-12-07 13:36:19 -0300

    Update UsuarioGUI.java

--- src/main/java/com/academia/view/UsuarioGUI.java
+++ src/main/java/com/academia/view/UsuarioGUI.java
@@ -6,10 +6,21 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JPasswordField;
+import javax.swing.JRadioButton;
+import javax.swing.JButton;
 
 public class UsuarioGUI extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textFieldName;
+	private JTextField textFieldEmail;
+	private JPasswordField passwordFieldPassword;
 
 	/**
 	 * Launch the application.
@@ -32,11 +43,91 @@ public class UsuarioGUI extends JFrame {
 	 */
 	public UsuarioGUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 894, 446);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		
+		JLabel lblName = new JLabel("Nome:");
+		
+		textFieldName = new JTextField();
+		textFieldName.setColumns(10);
+		
+		JLabel lblEmail = new JLabel("Email:");
+		
+		textFieldEmail = new JTextField();
+		textFieldEmail.setColumns(10);
+		
+		JLabel lblPassword = new JLabel("Senha:");
+		
+		passwordFieldPassword = new JPasswordField();
+		
+		JRadioButton rdbtnAtivo = new JRadioButton("Ativo");
+		
+		JRadioButton rdbtnAdmin = new JRadioButton("Adimistrador");
+		
+		JButton btnIncluir = new JButton("Incluir");
+		
+		JButton btnAlterar = new JButton("Alterar");
+		
+		JButton btnExcluir = new JButton("Excluir");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(59)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblName, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
+							.addGap(35)
+							.addComponent(textFieldName, GroupLayout.PREFERRED_SIZE, 575, GroupLayout.PREFERRED_SIZE))
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+								.addComponent(lblPassword)
+								.addComponent(lblEmail, GroupLayout.PREFERRED_SIZE, 28, GroupLayout.PREFERRED_SIZE))
+							.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(passwordFieldPassword)
+								.addComponent(textFieldEmail, GroupLayout.DEFAULT_SIZE, 575, Short.MAX_VALUE)
+								.addGroup(gl_contentPane.createSequentialGroup()
+									.addComponent(rdbtnAtivo, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE)
+									.addGap(52)
+									.addComponent(rdbtnAdmin, GroupLayout.PREFERRED_SIZE, 85, GroupLayout.PREFERRED_SIZE)
+									.addGap(92)
+									.addComponent(btnIncluir, GroupLayout.PREFERRED_SIZE, 61, GroupLayout.PREFERRED_SIZE)
+									.addGap(64)
+									.addComponent(btnAlterar, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+									.addGap(57)
+									.addComponent(btnExcluir, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
+									.addGap(153))))))
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(35)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addGap(3)
+							.addComponent(lblName))
+						.addComponent(textFieldName, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(53)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblEmail)
+						.addComponent(textFieldEmail, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(58)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblPassword)
+						.addComponent(passwordFieldPassword, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addGap(51)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(rdbtnAtivo)
+						.addComponent(rdbtnAdmin)
+						.addComponent(btnIncluir)
+						.addComponent(btnExcluir)
+						.addComponent(btnAlterar))
+					.addContainerGap(117, Short.MAX_VALUE))
+		);
+		contentPane.setLayout(gl_contentPane);
 	}
 
 }
